15a8f2777b5c8be79b81ee1101e5ed96f0d66cd8
LE2D22yanoura <k023g0120@g.neec.ac.jp>
2025-04-24 12:04:19 +0900

    基礎終わり

--- main.cpp
+++ main.cpp
@@ -1,7 +1,42 @@
 #include <Windows.h>
-
+#include <KamataEngine.h>
+#include "GameScene.h"
+using namespace KamataEngine;
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+	//エンジンの初期化
+	KamataEngine::Initialize(L"LE2D_16_ヤノウラ_ハルト_CG4");
 	
+	//メインループ
+	while (true) {
+
+		// DirectXCommonインスタンスの取得
+		DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+		//ゲームシーンのインスタンス生成
+		GameScene* gameScene = new GameScene();
+
+		//ゲームシーンの初期化
+		gameScene->Initialize();
+
+	//エンジンの更新
+		if (KamataEngine::Update()) {
+		break;
+		}
+		//ゲームシーンの更新
+		gameScene->Update();
+
+		//描画開始
+		dxCommon->PreDraw();
+
+		//ゲームシーンの描画
+		gameScene->Draw();
+
+		//描画終了
+		dxCommon->PostDraw();
+
+	}
+
+	//エンジンの終了処理
+	KamataEngine::Finalize();
 	return 0;
 }
